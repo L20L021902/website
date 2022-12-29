846ff6865b67708328f5a025f1993fea00e40d67
L20L021902 <L20L021902@stu.hit.edu.cn>
2022-12-29 06:20:04 +0300

    add getSales

--- src/main/java/letscode/GovernmentGoods.java
+++ src/main/java/letscode/GovernmentGoods.java
@@ -26,4 +26,43 @@ public class GovernmentGoods extends HttpServlet {
             out.write(content);
         }
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Check authorization
+        String username = TokenChecker.authenticate(req, resp);
+        if (username == null) { return; }
+
+        if (req.getPathInfo() == null) {
+            resp.sendError(404);
+            return;
+        }
+
+        switch (req.getPathInfo()) {
+            case "/get":
+                getSales(req, resp);
+                break;
+            default:
+                resp.sendError(404);
+                break;
+        }
+    }
+
+    private static void getSales(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            String json = Database.getSales();
+            if (json == null) {
+                resp.sendError(500);
+                return;
+            }
+
+            resp.setStatus(200);
+            resp.setContentType("application/json");
+            try (PrintWriter out = resp.getWriter()) {
+                out.write(json);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
